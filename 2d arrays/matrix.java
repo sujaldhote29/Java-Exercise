4818bc6ff7e0e061e7c2527d0bed422282927a08
Sujal Dhote <sujaldhote1@gmail.com>
2024-01-24 12:50:10 +0530

    matrix

--- 2d arrays/matrix.java
+++ 2d arrays/matrix.java
@@ -1,6 +1,10 @@
 
 public class matrix {
+    public static int search(int n[][]){
+
+    }
     public static void main(String[] args) {
-        
+        int arr[][] = {{10,20,30,40},{21,23,25,36},{31,34,38,40}};
+
     }
 }
