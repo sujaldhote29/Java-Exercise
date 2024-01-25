b3bf6bc6549881699a974934608c2bf27cf545ce
Sujal Dhote <sujaldhote1@gmail.com>
2024-01-25 17:40:12 +0530

    patterns

---
+++ patterns/number_tri.java
@@ -0,0 +1,20 @@
+
+public class number_tri {
+    public static void main(String args[]){
+        int n = 4;
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<=i; j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+        // System.out.println();
+
+        for(int i = n; i>=0;i--){
+            for(int j = 1; j<=i;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
