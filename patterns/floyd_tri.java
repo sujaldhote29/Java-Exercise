9dc987862d416b6df7934245442e4aef0b110462
Sujal Dhote <sujaldhote1@gmail.com>
2024-02-12 14:56:17 +0530

    yes

---
+++ patterns/floyd_tri.java
@@ -0,0 +1,14 @@
+public class floyd_tri {
+    public static void main(String args[]){
+        int n = 6;
+        int num =1;
+        char c = 'A';
+        for(int i =1; i<=n; i++){
+            for(int j = 1;j<=n-i+1; j++){
+                System.out.print(c+" ");
+                c++;
+            }
+            System.out.println();
+        }
+    }
+}
