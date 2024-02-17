6db6317006ff356839b08af480af8a98a5ec1477
Sujal Dhote <sujaldhote1@gmail.com>
2024-02-17 14:42:56 +0530

    practice

--- patterns/floyd_tri.java
+++ patterns/floyd_tri.java
@@ -1,7 +1,7 @@
 public class floyd_tri {
     public static void main(String args[]){
         int n = 6;
-        int num =1;
+        // int num =1;
         char c = 'C';
         for(int i =1; i<=n; i++){
             for(int j = 1;j<=n-i+1; j++){
