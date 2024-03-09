fc5ba4c6856e025c024f9c679804d6641cec5407
Sujal <sujaldhote1@gmail.com>
2024-03-09 14:27:17 +0530

    reverse

---
+++ 2d arrays/input.java
@@ -0,0 +1,75 @@
+import java.util.*;
+public class input {
+
+    public static int rowsum(int[][] arr ){
+        int minsum = Integer.MAX_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            int sum =0;
+            for (int j = 0; j < arr[i].length; j++) {
+                sum+= arr[i][j];
+            }
+            if (minsum>sum) {
+                minsum = sum;
+            }
+        }
+
+        return minsum;
+    }
+    public static void wavecolpattern(int[][] arr){
+       
+        for (int j = 0; j < arr.length; j++) {
+            if (j%2==1) {
+                for (int i = arr.length-1; i >= 0; i--) {
+                    System.out.print(arr[i][j]+" ");
+                }
+                // System.out.println();
+            }else{
+                for (int i = 0; i < arr.length; i++) {
+                    System.out.print(arr[i][j]+" ");
+                }
+                // System.out.println();
+            }
+        }
+       
+    }
+ public static void spiral(int[][] arr){
+    int startrow = 0;
+    int endrow = arr.length-1;
+    int startcol = 0;
+    int endcol = arr[0].length-1;
+
+    while (startrow<=endrow && startcol<= endcol) {
+        for(int i =startcol; i<=endcol; i++)
+        System.out.print(arr[i][endcol]+" ");
+    }
+
+    for (int i = startrow; i <=endrow; i++) {
+        System.out.print(arr[i][endrow]+" ");
+    }
+
+    for (int i = endcol-1; i >=startcol; i--) {
+        System.out.println();
+    }
+ }   
+    public static void main(String args[]){
+        int arr[][] = new int[3][3];
+        Scanner sc = new Scanner(System.in);
+
+        for(int i =0; i<arr.length; i++){
+            for(int j =0; j<arr[i].length; j++){
+                System.out.print("["+i+","+j+"]"+" = ");
+                 arr[i][j] = sc.nextInt();
+            }
+        }
+
+        // for (int i = 0; i < arr.length; i++) {
+        //     for (int j = 0; j < arr[i].length; j++) {
+        //         System.out.print(arr[i][j]+" ");
+        //     }
+        //     System.out.println();
+        // }
+
+            // System.out.println(rowsum(arr));
+            wavecolpattern(arr);
+    }
+}
--- arrays/reverse.java
+++ arrays/reverse.java
@@ -1,27 +1,86 @@
 public class reverse {
-    public void array(int arr[]){
+    public static void array(int arr[]) {
         // int n = arr.length-1;
         int start = 0;
-        int end = arr.length-1;
-        while (start<end) {
-            int temp =arr[start];
+        int end = arr.length - 1;
+        while (start < end) {
+            int temp = arr[start];
             arr[start] = arr[end];
             arr[end] = temp;
             start++;
-            end --;
+            end--;
+        }
+    }
+
+    public static void indexRev(int[] arr, int idx) {
+        int start = idx + 1;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void mergeSortedArr(int arr1[], int arr2[], int[] arr3) {
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while (i < arr1.length && j < arr2.length) {
+            if (arr1[i] < arr2[j]) {
+                arr3[k++] = arr1[i++];
+            } else {
+                arr3[k++] = arr2[j++];
+            }
         }
 
+        while (i < arr1.length) {
+            arr3[k++] = arr1[i++];
+        }
 
-
+        while (j < arr2.length) {
+            arr3[k++] = arr2[j++];
+        }
 
     }
-    public static void main(String[] args) {
-        int arr[] = {2,3,4,2,45,67};
-        reverse  a= new reverse();
-        a.array(arr);
 
-        for(int i = 0; i<arr.length; i++){
-            System.out.print(arr[i]+" ");
+    public static void printArr(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
         }
+    }
+    public static void place_zero_at_right(int arr[]){
+        int i =0;
+        for(int j = 0; j<arr.length; j++){
+            if (arr[j]!=0) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        // int arr[] = { 2, 3, 4, 2, 45, 67 };
+        // reverse a = new reverse();
+        // array(arr);
+        // printArr(arr);
+        // System.out.println();
+        // indexRev(arr, 3);
+        // printArr(arr);
+
+        // int[] arr1 = { 1, 3, 5, 6, 7, 9 };
+        // int[] arr2 = { 2, 4, 8 };
+        // int arr3[] = new int[arr1.length + arr2.length];
+
+        // mergeSortedArr(arr1, arr2, arr3);
+        // printArr(arr3);
+
+        int arr[] = { 20,0,0,23,23,44};
+        place_zero_at_right(arr);
+        printArr(arr);
     }
 }
