0cef2587a15ca6307237d21dfcbc65b99610a22f
Sujal <sujaldhote1@gmail.com>
2024-03-12 18:09:08 +0530

    palindrome

--- pal.java
+++ pal.java
@@ -1,3 +1,21 @@
+import java.util.*;
 public class pal {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the String = ");
+        String str = sc.next();
+        boolean is = true;
+        for (int i = 0; i < str.length()/2; i++) {
+            if (str.charAt(i)!=str.charAt(str.length()-i-1)) {
+               is = false;
+               break;
+            }
+        }
+        if (is) {
+            System.out.println("Palindrome");
+        }else{
+            System.out.println("Not");
+        }
+        sc.close();
+    }
 }
