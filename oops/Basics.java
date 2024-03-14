3a6fc6b274eb9459bc9c19362d50368619371af1
Sujal <sujaldhote1@gmail.com>
2024-03-14 18:00:30 +0530

    Basics

--- oops/Basics.java
+++ oops/Basics.java
@@ -1,8 +1,13 @@
 public class Basics {
     public static void main(String[] args) {
-        Student s1 = new Student("Sujal", 17, 95.8f);
-        Student s2 = new Student("Raj", 21, 58.8f);
-        System.out.println(s1.marks);
+        // Student s1 = new Student("Sujal", 17, 95.8f);
+        // Student s2 = new Student("Raj", 21, 58.8f);
+        // System.out.println(s1.marks);
+        // System.out.println(s2.name);
+        Student s1 = new Student();
+        s1.setname("sujal");
+        s1.getname();
+        
     }
 }
 
@@ -11,10 +16,18 @@ public class Basics {
     int roll;
     float marks;
 
-    Student(String name, int roll,float marks){
+    // Student(String name, int roll,float marks){
+    //     this.name = name;
+    //     this.roll = roll;
+    //     this.marks = marks;
+    // }
+
+    public void setname(String name){
         this.name = name;
-        this.roll = roll;
-        this.marks = marks;
+    }
+
+    public void getname(){
+        System.out.println(name);
     }
 
 }
